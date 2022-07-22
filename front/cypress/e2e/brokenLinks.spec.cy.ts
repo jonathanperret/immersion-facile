24dee64e19ae4823d3ec01b1463d08f06465ee7e
Enguerran Weiss <enguerranweiss@gmail.com>
2022-07-22 10:41:40 +0200

    remove unused vars

--- front/cypress/e2e/brokenLinks.spec.cy.ts
+++ front/cypress/e2e/brokenLinks.spec.cy.ts
@@ -2,8 +2,7 @@ describe("Check for broken links", () => {
   it(`Check for 404`, () => {
     cy.visit("/");
     const links = cy.get("#footer a, .fr-header a");
-    links.each(($link, index) => {
-      const linkText = $link.text();
+    links.each(($link) => {
       const href = $link.attr("href");
       cy.request($link.attr("href")).should((response) => {
         cy.log(`${href}: ${response.status}`);
