e94e22f5080047b125dd1a2afcd537e95e950191
Enguerran Weiss <enguerranweiss@gmail.com>
2022-07-22 14:53:08 +0200

    rm demo.spec, better check link request status > 199 && < 400

--- front/cypress/e2e/brokenLinks.spec.cy.ts
+++ front/cypress/e2e/brokenLinks.spec.cy.ts
@@ -1,12 +1,12 @@
 describe("Check for broken links", () => {
-  it(`Check for 404`, () => {
+  it(`Check for links in error`, () => {
     cy.visit("/");
-    const links = cy.get("#footer a, .fr-header a");
+    const links = cy.get(".fr-footer a, .fr-header a");
     links.each(($link) => {
       const href = $link.attr("href");
       cy.request($link.attr("href")).should((response) => {
         cy.log(`${href}: ${response.status}`);
-        expect(response.status).to.not.eq(404);
+        expect(response.status).to.be.greaterThan(199).and.lessThan(400);
       });
     });
   });
