7090c71723bae9e8e145f629b8314090b701e067
Enguerran Weiss <enguerranweiss@gmail.com>
2022-07-22 09:58:36 +0200

    add cypress broken links on header / footer test

---
+++ front/cypress/e2e/brokenLinks.spec.cy.ts
@@ -0,0 +1,14 @@
+describe("Check for broken links", () => {
+  it(`Check for 404`, () => {
+    cy.visit("/");
+    const links = cy.get("#footer a, .fr-header a");
+    links.each(($link, index) => {
+      const linkText = $link.text();
+      const href = $link.attr("href");
+      cy.request($link.attr("href")).should((response) => {
+        cy.log(`${href}: ${response.status}`);
+        expect(response.status).to.not.eq(404);
+      });
+    });
+  });
+});
