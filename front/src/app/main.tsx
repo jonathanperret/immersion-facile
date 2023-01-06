3e4e6f54fbd8dbb1e4f1908abb09afedb464f513
Enguerran Weiss <enguerranweiss@gmail.com>
2023-01-06 09:43:59 +0100

    update createRoot call

--- front/src/app/main.tsx
+++ front/src/app/main.tsx
@@ -3,7 +3,6 @@ import "src/assets/dsfr/dsfr.min.css";
 import "src/assets/dsfr/utility/icons/icons.min.css";
 
 import React from "react";
-import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { App } from "src/app/App";
 import { store } from "src/config/dependencies";
@@ -12,9 +11,13 @@ import { MetaContent } from "./components/layout/MetaContent";
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 
 startReactDsfr({ defaultColorScheme: "light" });
-import { RouteProvider } from "./routes/routes";
 
-ReactDOM.render(
+import { RouteProvider } from "./routes/routes";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container as HTMLElement);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouteProvider>
@@ -23,5 +26,4 @@ ReactDOM.render(
       </RouteProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root"),
 );
